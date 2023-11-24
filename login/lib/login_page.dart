b15b0e7cab0da9051dcfd0b1f859c4748d18851f
Govind Kumar Yadav <robertgovind@gmail.com>
2023-11-24 14:13:52 +0545

    second commit

--- login/lib/login_page.dart
+++ login/lib/login_page.dart
@@ -1,13 +1,107 @@
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Animation<double> _iconAnimation;
+
+  @override
+  void initState() {
+    _animationController = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 1),
+    );
+    _iconAnimation = CurvedAnimation(
+      curve: Curves.bounceOut,
+      parent: _animationController,
+    );
+    _iconAnimation.addListener(() => setState(() {}));
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Login page'),
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          Image(
+            image: const AssetImage('assets/bg.jpg'),
+            fit: BoxFit.cover,
+            color: Colors.blueAccent.withOpacity(0.8),
+            colorBlendMode: BlendMode.color,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                const Image(
+                  image: AssetImage('assets/operation.png'),
+                  height: 200,
+                ),
+                TextField(
+                  decoration: InputDecoration(
+                    labelText: 'Enter Email',
+                    labelStyle: const TextStyle(color: Colors.white),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                  ),
+                  keyboardType: TextInputType.emailAddress,
+                ),
+                const SizedBox(
+                  height: 8,
+                ),
+                TextField(
+                  decoration: InputDecoration(
+                    labelText: 'Enter password',
+                    labelStyle: const TextStyle(color: Colors.white),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                  ),
+                  obscureText: true,
+                  keyboardType: TextInputType.text,
+                ),
+                ElevatedButton(
+                  style: ButtonStyle(
+                      backgroundColor: MaterialStateColor.resolveWith(
+                          (states) => Colors.blueAccent)),
+                  onPressed: () {},
+                  child: const Text(
+                    'Login',
+                    style: TextStyle(
+                        color: Colors.white, fontSize: 18, letterSpacing: 1),
+                  ),
+                ),
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      'Don\'t have an account',
+                      style: TextStyle(
+                          color: Colors.white, fontSize: 18, letterSpacing: 1),
+                    ),
+                    Text(
+                      'Signup',
+                      style: TextStyle(
+                          color: Colors.white, fontSize: 18, letterSpacing: 1),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
