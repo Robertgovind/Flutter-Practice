8c61945ac61c0696434c1862971bf43e5dd73eca
Govind Kumar Yadav <robertgovind@gmail.com>
2023-11-25 20:46:00 +0545

    initial phase

--- chat/lib/home.dart
+++ chat/lib/home.dart
@@ -1,10 +1,62 @@
 import 'package:flutter/material.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   const Home({super.key});
 
   @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  TextEditingController message = TextEditingController();
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.lightBlueAccent,
+      appBar: AppBar(
+        backgroundColor: Colors.lightBlueAccent,
+        title: const Text(
+          'Chat App',
+          textAlign: TextAlign.center,
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 12),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    decoration: InputDecoration(
+                      labelText: 'Enter message',
+                      labelStyle:
+                          const TextStyle(fontSize: 18, color: Colors.white),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                    ),
+                    controller: message,
+                  ),
+                ),
+                const SizedBox(
+                  width: 8,
+                ),
+                IconButton(
+                  onPressed: () {
+                    message.clear();
+                  },
+                  icon: const Icon(
+                    Icons.send,
+                    size: 35,
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
